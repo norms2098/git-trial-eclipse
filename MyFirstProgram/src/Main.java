7e4af18290bc38fdeb8736678ec51c70a6049145
norms2098 <nrmsdgdgn@gmail.com>
2024-02-17 23:45:47 +0800

    added last line of println statement

--- MyFirstProgram/src/Main.java
+++ MyFirstProgram/src/Main.java
@@ -4,5 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
 		System.out.print("hello worlds!");
+		System.out.print("hello worlds!3");
+
 	}
 }
